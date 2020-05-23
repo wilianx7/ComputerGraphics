11831d0427c188478a61557d5113c300a783af72
wilianx7 <wilianx7@hotmail.com>
2020-05-23 14:50:06 -0300

    Add Airplane model and resource

---
+++ src/app/resources/airplane.ts
@@ -0,0 +1,10 @@
+export class Airplane {
+    public id: number;
+    public x: number;
+    public y: number;
+    public radius: number;
+    public angle: number;
+    public speed: number;
+    public direction: number;
+    public translation: string;
+}
